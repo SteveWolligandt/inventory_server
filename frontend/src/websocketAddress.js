5f803ef2ef9dfd4c21097752ccbfb64f7a8bb225
Steve Wolligandt <stevewolligandt@gmail.com>
2024-09-05 08:39:58 +0200

    websocket ssl flag

--- frontend/src/websocketAddress.js
+++ frontend/src/websocketAddress.js
@@ -1,14 +1,25 @@
 
 export default function websocketAddress() {
   var loc = window.location, new_uri;
-  new_uri = 'wss://';
-  new_uri += loc.hostname;
-  new_uri += ':';
-  if (loc.port === '3000') {
-    new_uri += '443';
-  } else {
-    new_uri += loc.port;
+
+  var useSSL = true;
+  if (useSSL) {
+    new_uri = 'wss://';
+    new_uri += loc.hostname;
+    new_uri += ':';
+    if (loc.port === '3000') {
+      new_uri += '443';
+    } else {
+      new_uri += loc.port;
+    }
+    new_uri += loc.pathname + 'ws';
+    return new_uri;
   }
-  new_uri += loc.pathname + 'ws';
+  else {
+    new_uri = 'ws://';
+    new_uri += loc.hostname;
+    new_uri += ':8080';
+    new_uri += loc.pathname + 'ws';
+  }
   return new_uri;
 }
